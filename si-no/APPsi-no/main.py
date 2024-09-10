2c24f406aef35948c6ffce2737885c3c993e273d
antony6654 <calixtoapolinarmarcoantonio@cetis50cdmx.com>
2024-09-09 20:18:49 -0600

    1.0

---
+++ si-no/APPsi-no/main.py
@@ -0,0 +1,36 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title="¿Me perdonas?"
+    page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment=ft.MainAxisAlignment.CENTER
+    page.bgcolor="red"
+    
+    lbll=ft.Text("¿Me perdonas?",
+                style=ft.TextStyle(size=40,weight="boold"))
+    
+    Ingl=ft.Image(src="triste.png",width=200,height=200)
+    
+    btn5i=ft.ElevatedButton("Si",
+                            color="grenn",
+                            width=100,
+                            height=50)
+    
+    btnNo=ft.ElevatedButton("No",
+                            color="red",
+                            width=100,
+                            height=50)
+    
+    page.add(
+        ft.Column(
+            [
+                lbll,
+                Ingl,
+                ft.Row([btn5i,btnNo])
+            ]
+        )
+    )
+
+
+ft.app(main)
